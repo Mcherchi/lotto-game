08eb3407ed309de9cace1162de7e4c4a18d31659
Mattia_Cherchi <mattiacherchi93@gmail.com>
2023-05-03 17:19:58 +0200

    Added messages object

---
+++ View/messages.js
@@ -0,0 +1,21 @@
+// Object containing message strings used in the Lotto Game
+const messages = {
+  welcome: "Welcome to the Lotto Game!",
+  goodbye: "Goodbye!",
+  howManyTicket:
+    "How many tickets do you want to generate? (min: 1 - max: 5, 0: exit)",
+  howManyNumbers:
+    "How many numbers do you want to generate? (min: 1 - max: 10)",
+  error: "Invalid Input!",
+  choice: "Enter you choice: ",
+  confirm: "Confirm? (Y/N)",
+  youHaveChosen: "You have chosen:",
+  chooseType: 'Choose a type (or "c" for the next step):',
+  warningMessageType: "You must choose at least one type.",
+  chooseCity: 'Choose a city (or "c" for the next step):',
+  warningMessageCity: "You must choose at least one city.",
+  continue: "Enter c to proceed to the next step",
+  warningConfirmCoiche: 'Invalid input. Please enter "Y" or "N".',
+};
+
+module.exports = messages;
